fb5aece862535a864f480b7a0d033d644da683d9
Emie-Trialer <emiedonmukumo@gmail.com>
2025-05-14 00:02:29 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,12 +8,33 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// set up multer for file upload to memory
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  const file = req.file;
 
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  console.log(file);
+
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
